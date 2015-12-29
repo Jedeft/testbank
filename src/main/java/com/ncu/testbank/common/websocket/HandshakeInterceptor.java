8df1093fa36efcf7ef916b777f05aafdf85f145a
Administrator <Administrator@Jedeft-PC>
2015-12-29 17:46:46 +0800

    websocket环境搭建，待完成通知消息

---
+++ src/main/java/com/ncu/testbank/common/websocket/HandshakeInterceptor.java
@@ -0,0 +1,27 @@
+package com.ncu.testbank.common.websocket;
+
+import java.util.Map;
+
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.web.socket.WebSocketHandler;
+import org.springframework.web.socket.server.support.HttpSessionHandshakeInterceptor;
+
+
+public class HandshakeInterceptor extends HttpSessionHandshakeInterceptor {
+
+	@Override
+	public boolean beforeHandshake(ServerHttpRequest request,
+			ServerHttpResponse response, WebSocketHandler wsHandler,
+			Map<String, Object> attributes) throws Exception {
+		return super.beforeHandshake(request, response, wsHandler, attributes);
+	}
+
+	@Override
+	public void afterHandshake(ServerHttpRequest request,
+			ServerHttpResponse response, WebSocketHandler wsHandler,
+			Exception ex) {
+		super.afterHandshake(request, response, wsHandler, ex);
+	}
+
+}
