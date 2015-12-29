2bf1ab96b64023184361a2407b9068f8f8d02345
jedeft <jedeft@163.com>
2015-12-29 23:38:49 +0800

    搭建websocket解决通知问题雏形 需要解决的问题：消息推送路由确定 在用户信息缓存中增加用户状态判读消息推送模块是否可用，即标记用户的websocket链接是否建立成功，用户登录成功后正常功能的使用和消息推送是两个独立模块，互不影响

---
+++ src/main/java/com/ncu/testbank/base/config/WebSocketConfig.java
@@ -0,0 +1,29 @@
+package com.ncu.testbank.base.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.AbstractWebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+
+//在spring-mvc.xml中配置了<mvc:annotation-driven />后，此处不用再写此注解。会报错
+//@EnableWebMvc
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer {
+
+	@Override
+	public void configureMessageBroker(MessageBrokerRegistry config) {
+		//客户端(浏览器)接收推送消息的url地址开头部分
+		config.enableSimpleBroker("/client/messages");
+		
+		//服务器辨别是否是一个websocket请求。凡http://ip:port/projectName/websocket/*类的url，都为websocket请求
+		config.setApplicationDestinationPrefixes("/websocket");
+	}
+
+	//客户端建立websocket连接的url请求地址
+	public void registerStompEndpoints(StompEndpointRegistry registry) {
+		registry.addEndpoint("/websocket/messages").withSockJS();
+	}
+
+}
