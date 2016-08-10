8971188b801bb251b55deb70b2edb8a3d73c7b77
Jedeft <Jedeft@163.com>
2016-08-10 17:11:36 +0800

    add to index解决冲突

--- src/main/java/com/ncu/testbank/background/TaskJob.java
+++ src/main/java/com/ncu/testbank/background/TaskJob.java
@@ -91,7 +91,7 @@ public class TaskJob {
 			connection.start();
 			session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
 			// 获取服务器上的消息
-			destination = session.createQueue("examQueue");
+			destination = session.createQueue(properties.getProperty("queueName"));
 			consumer = session.createConsumer(destination);
 			while (true) {
 				//接收者接收消息的时间，这里为100秒
@@ -120,12 +120,11 @@ public class TaskJob {
 						EmailUtils.sendEmail(teacher.getEmail(), "组卷失败的同学学号为："
 								+ failStudents.toString() + "；请及时重新组卷，避免影响考试！");
 					}
-				} else {
-					break;
 				}
 			}
-		} catch (JMSException e) {
+		} catch (Exception e) {
 			// TODO 后台程序崩溃，邮件通知管理员~
+			// Spring task的机制为，若当前作业抛出异常那么就会停止当前作业处理，并且不再调用后续其他作业的执行
 			log.error(e);
 		} finally {
 			try {
